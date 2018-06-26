4e0ffbdb350212c6c84a8cf9094b4e5070e0a471
Shlomi.Wolman <shlomi.wolman@clicksoftware.com>
2018-06-26 15:08:39 +0300

    Adding one more 0 for commit change

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace MyTest
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 1000; i++)
             {
                 Console.WriteLine(i);
             }
